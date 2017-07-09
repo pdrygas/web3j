045851f194bdc6f535c00e127d8fb010af657630
Pawel Drygas <drygaspawel@gmail.com>
2017-07-09 21:33:08 +0100

    Deleted method signatures that were moved to Personal interface, declared extending it.

--- src/main/java/org/web3j/protocol/parity/Parity.java
+++ src/main/java/org/web3j/protocol/parity/Parity.java
@@ -1,28 +1,26 @@
 package org.web3j.protocol.parity;
 
-import java.math.BigInteger;
 import java.util.Map;
 
 import org.web3j.crypto.WalletFile;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.Web3jService;
+import org.web3j.protocol.admin.Personal;
+import org.web3j.protocol.admin.methods.response.NewAccountIdentifier;
+import org.web3j.protocol.admin.methods.response.PersonalEcRecover;
 import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.request.Transaction;
 import org.web3j.protocol.core.methods.response.VoidResponse;
-import org.web3j.protocol.parity.methods.response.NewAccountIdentifier;
 import org.web3j.protocol.parity.methods.response.PersonalAccountsInfo;
-import org.web3j.protocol.parity.methods.response.PersonalEcRecover;
-import org.web3j.protocol.parity.methods.response.PersonalListAccounts;
 import org.web3j.protocol.parity.methods.response.PersonalRejectRequest;
 import org.web3j.protocol.parity.methods.response.PersonalRequestsToConfirm;
 import org.web3j.protocol.parity.methods.response.PersonalSign;
 import org.web3j.protocol.parity.methods.response.PersonalSignerEnabled;
-import org.web3j.protocol.parity.methods.response.PersonalUnlockAccount;
 
 /**
  * JSON-RPC Request object building factory for Parity.
  */
-public interface Parity extends Web3j {
+public interface Parity extends Web3j, Personal {
     static Parity build(Web3jService web3jService) {
         return new JsonRpc2_0Parity(web3jService);
     }
@@ -31,21 +29,10 @@ public interface Parity extends Web3j {
 
     Request<?, PersonalSign> personalSign(String hexMessage, String accountId, String password);
 
-    Request<?, PersonalEcRecover> personalEcRecover(String hexMessage, String signedMessage);
-
-    Request<?, PersonalListAccounts> personalListAccounts();
-
-    Request<?, NewAccountIdentifier> personalNewAccount(String password);
-
     Request<?, NewAccountIdentifier> personalNewAccountFromPhrase(String phrase, String password);
 
     Request<?, NewAccountIdentifier> personalNewAccountFromWallet(
             WalletFile walletFile, String password);
-
-    Request<?, PersonalUnlockAccount> personalUnlockAccount(
-            String accountId, String password, BigInteger duration);
-
-    Request<?, PersonalUnlockAccount> personalUnlockAccount(String accountId, String password);
 
     Request<?, org.web3j.protocol.core.methods.response.EthSendTransaction>
             personalSignAndSendTransaction(
@@ -64,4 +51,6 @@ public interface Parity extends Web3j {
             String requestId, Transaction transaction, String password);
 
     Request<?, PersonalRejectRequest> personalRejectRequest(String requestId);
+
+    Request<?, PersonalEcRecover> personalEcRecover(String hexMessage, String signedMessage);
 }
