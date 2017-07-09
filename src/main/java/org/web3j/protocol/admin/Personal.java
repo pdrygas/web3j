cf64338319cf9d78c110e7391f80cf6bd3cbd16e
Pawel Drygas <drygaspawel@gmail.com>
2017-07-09 21:11:38 +0100

    Defined shared personal calls

--- src/main/java/org/web3j/protocol/admin/Personal.java
+++ src/main/java/org/web3j/protocol/admin/Personal.java
@@ -1,4 +1,25 @@
 package org.web3j.protocol.admin;
 
+import java.math.BigInteger;
+
+import org.web3j.protocol.admin.methods.response.NewAccountIdentifier;
+import org.web3j.protocol.admin.methods.response.PersonalListAccounts;
+import org.web3j.protocol.admin.methods.response.PersonalUnlockAccount;
+import org.web3j.protocol.core.Request;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+
 public interface Personal {
+    Request<?, PersonalListAccounts> personalListAccounts();
+
+    Request<?, NewAccountIdentifier> personalNewAccount(String password);
+
+    Request<?, PersonalUnlockAccount> personalUnlockAccount(
+            String address, String passphrase);
+
+    Request<?, PersonalUnlockAccount> personalUnlockAccount(
+            String address, String passphrase, BigInteger duration);
+
+    Request<?, EthSendTransaction> personalSendTransaction(
+            Transaction transaction, String passphrase);
 }
